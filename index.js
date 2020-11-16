e83c5023fc033de48b3c2048adf85116e873254a
S  1 <lokohopo@gmail.com>
2020-11-16 23:26:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,15 +20,3 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name]
 }
-
-funcion prependKitten(name) {
-  return [name, ...kittens]
-}
-
-function removeLastKitten() {
-  return kittens.slice(0, kittens.length -1)
-}
-
-function removeFirstKitten() {
-  return kittens.slice (1)
-}
