23466c710a638f2d6722d6346469b326da455f85
S  1 <lokohopo@gmail.com>
2020-11-16 23:23:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,17 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  return kittens.append(name)
+  return [...kittens, name]
+}
+
+funcion prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice (1)
 }
